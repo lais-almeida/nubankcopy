32a419720bf3815fbc353747bf0b79db74002b8b
lais-almeida <lais.fernanda.almeida@gmail.com>
2020-09-10 17:35:28 -0300

    first commit

---
+++ src/components/Header/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-component/native';
+
+export const Container = styled.View`
+	aligne-items: center;
+	padding: 40px 0 30px;
+`;
---
+++ src/pages/Main/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
+import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <>
+      <Container />
+      <StatusBar style="light" backgroundColor- "#8B10AE"/>
+    </>
+  )
+}
